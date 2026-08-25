e75d9e63e0091eaa736d04c77f9d009d6a76cb1d
Gaurang Sharma <m.gaurangsharma@gmail.com>
2026-08-25 23:12:06 +0530

    Break a test to see Jenkins fail

--- test_cart.py
+++ test_cart.py
@@ -1,7 +1,7 @@
 from cart import cart_total, apply_discount
 def test_cart_total():
  items = [{"price": 100, "qty": 2}, {"price": 50, "qty": 1}]
- assert cart_total(items) == 250
+ assert cart_total(items) == 999
 def test_apply_discount():
  assert apply_discount(200, 10) == 180
 def test_zero_discount():
