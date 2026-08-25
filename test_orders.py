d7ee891d406c375b9b11778d7d0074371b256436
Gaurang Sharma <m.gaurangsharma@gmail.com>
2026-08-26 02:51:12 +0530

    Fix the test

--- test_orders.py
+++ test_orders.py
@@ -1,6 +1,6 @@
 from orders import order_total, is_free_delivery
 def test_order_total():
-    assert order_total(250, 40) == 999
+    assert order_total(250, 40) == 290
 def test_free_delivery_yes():
     assert is_free_delivery(500) == True
 def test_free_delivery_no():
